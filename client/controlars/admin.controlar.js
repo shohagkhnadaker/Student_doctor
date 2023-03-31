e70775c5a818d786c406fa2f7219e2fdbeeb583c
Shohag <tashohag1@gmail.com>
2023-04-01 01:19:24 +0300

    ho
    yes

---
+++ client/controlars/admin.controlar.js
@@ -0,0 +1,89 @@
+const userModule=require('../modules/User.module')
+const doctorModule=require('../modules/Doctor.module')
+
+
+const getAlluser=async(req,res)=>{
+    try {
+        const users=await userModule.find({})
+if (users) {
+    
+    return res.status(200).send({
+        success:true,
+        message:"get user successfull",
+        data:users
+    })
+}else{
+    
+    return res.status(200).send({
+        success:false,
+        message:"get user unsccessfull",
+    })
+}
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success:false,
+            message:'something wrong'
+        })
+    }
+}
+const getAlldoctors=async(req,res)=>{
+    try {
+        
+        const doctors=await doctorModule.find({})
+     if (doctors) {
+      return  res.status(200).send({
+            success:true,
+            message:"get doctor successfull",
+            data:doctors
+        })
+     } else {
+        return  res.status(200).send({
+            success:false,
+            message:"get doctor unsuccessfull",
+        })
+     }
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success:false,
+            message:'something wrong'
+        })
+    }
+}
+
+
+//change Doctor status
+const changeDoctorstatus=async(req,res)=>{
+try {
+const {doctorId,status}=req.body
+
+const doctor=await doctorModule.findByIdAndUpdate(doctorId,{status})
+
+const user=await userModule.findOne({_id:doctor.userId})
+const notification=user.notification
+
+notification.push({
+    type:"doctor status approved",
+    message:`your Doctor requirst has ${status}`,
+    onClickpath:'/notification'
+})
+user.isDoctor=true;
+await user.save();
+
+res.status(200).send({
+    success:true,
+    message:"doctor Status successfully approved",
+    data:doctor
+})
+    
+} catch (error) {
+    console.log(error);
+    res.status(500).send({
+        message:"somthing problem"
+    })
+}
+}
+module.exports={getAlldoctors,getAlluser,changeDoctorstatus}
